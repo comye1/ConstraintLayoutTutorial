d35a0dc1072f630b7df840447494d9a20e65c068
comye1 <yewon2664@gmail.com>
2021-05-26 23:58:31 +0900

    placeholder

--- app/src/main/java/com/example/constraintlayouttutorial/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayouttutorial/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.constraintlayouttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Placeholder
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layout : ConstraintLayout
+    private lateinit var placeHolder : Placeholder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout9)
+
+        layout = findViewById(R.id.layout)
+        placeHolder = findViewById(R.id.placeholder)
+    }
+
+    fun swapView(v : View) {
+        TransitionManager.beginDelayedTransition(layout)
+        placeHolder.setContentId(v.id)
     }
 }
